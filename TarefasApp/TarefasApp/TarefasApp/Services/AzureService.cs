8e26eb558d8ef1614ed26a0f803c90f2f81fcf4d
Núbio Knupp <nubioknupp@gmail.com>
2017-05-30 00:48:16 +0000

    Configurando o projeto para trabalhar com Azure Easy Tables

--- TarefasApp/TarefasApp/TarefasApp/Services/AzureService.cs
+++ TarefasApp/TarefasApp/TarefasApp/Services/AzureService.cs
@@ -1,7 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using Plugin.Connectivity;
 using TarefasApp.Authentication;
 using TarefasApp.Helpers;
+using TarefasApp.Models;
 using TarefasApp.Services;
 using Xamarin.Forms;
 
@@ -11,29 +19,47 @@ namespace TarefasApp.Services
 {
     public class AzureService
     {
-        private const string AppUrl = "https://tarefasapps.azurewebsites.net";
+        private MobileServiceClient _client;
+        private IMobileServiceSyncTable<Categoria> _categoriaTable;
+        private IMobileServiceSyncTable<Tarefa> _tarefaTable;
 
-        public MobileServiceClient Client { get; private set; }
-        public static bool UseAuth { get; set; } = false;
-
-        public void Initialize()
+        private async Task Initialize()
         {
-            Client = new MobileServiceClient(AppUrl);
+            _client = new MobileServiceClient("https://tarefasapps.azurewebsites.net");
+
             if (!string.IsNullOrWhiteSpace(Settings.AuthToken) && !string.IsNullOrWhiteSpace(Settings.UserId))
             {
-                Client.CurrentUser = new MobileServiceUser(Settings.UserId)
+                _client.CurrentUser = new MobileServiceUser(Settings.UserId)
                 {
                     MobileServiceAuthenticationToken = Settings.AuthToken
                 };
             }
+
+            //InitialzeDatabase for path
+            var path = "syncstore.db";
+            path = Path.Combine(MobileServiceClient.DefaultDatabasePath, path);
+
+            //setup our local sqlite store and intialize our table
+            var store = new MobileServiceSQLiteStore(path);
+
+            //Define table
+            store.DefineTable<Categoria>();
+            store.DefineTable<Tarefa>();
+
+            //Initialize SyncContext
+            await _client.SyncContext.InitializeAsync(store);
+
+            //Get our sync table that will call out to azure
+            _categoriaTable = _client.GetSyncTable<Categoria>();
+            _tarefaTable = _client.GetSyncTable<Tarefa>();
         }
 
         public async Task<bool> LoginAsync()
         {
-            Initialize();
+            await Initialize();
 
             var auth = DependencyService.Get<IAuthentication>();
-            var user = await auth.LoginAsync(Client, MobileServiceAuthenticationProvider.Facebook);
+            var user = await auth.LoginAsync(_client, MobileServiceAuthenticationProvider.Facebook);
 
             if (user == null)
             {
@@ -53,8 +79,54 @@ namespace TarefasApp.Services
             Settings.UserId = user.UserId;
 
             return true;
-
         }
+
+        //public async Task SyncCategoria()
+        //{
+        //    try
+        //    {
+        //        if (!CrossConnectivity.Current.IsConnected) return;
+
+        //        await _categoriaTable.PullAsync("allCategoria", _categoriaTable.CreateQuery());
+
+        //        await _client.SyncContext.PushAsync();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine("Unable to sync coffees, that is alright as we have offline capabilities: " + ex);
+        //    }
+
+       // }
+
+        //public async Task<IEnumerable<CupOfCoffee>> GetCoffees()
+        //{
+        //    //Initialize & Sync
+        //    await Initialize();
+        //    await SyncCoffee();
+
+        //    return await coffeeTable.OrderBy(c => c.DateUtc).ToEnumerableAsync(); ;
+
+        //}
+
+        //public async Task<CupOfCoffee> AddCoffee(bool atHome)
+        //{
+        //    await Initialize();
+
+        //    var coffee = new CupOfCoffee
+        //    {
+        //        DateUtc = DateTime.UtcNow,
+        //        MadeAtHome = atHome,
+        //        OS = Device.OS.ToString()
+        //    };
+
+        //    await coffeeTable.InsertAsync(coffee);
+
+        //    await SyncCoffee();
+        //    //return coffee
+        //    return coffee;
+        //}
+
+
 
     }
 }
